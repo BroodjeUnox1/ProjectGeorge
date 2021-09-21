034325f6c9bc96c198bf4b9e272653ffa08e882e
BroodjeUnox1 <stevenwoody7@gmail.com>
2021-09-21 14:59:55 +0200

    Added few elements

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
    		<!--  link & cdn's || bootstrap css -->
    		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
    		<!-- style.css -->
-   		<link rel="stylesheet" type="text/css" href="css/style.css">
+   		<link rel="stylesheet" type="text/css" href="style.css">
    		<!-- jQuery -->
    		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
    		<!-- popper -->
@@ -30,23 +30,36 @@
  	</head>
  	<body>
 
- 		<div class="container-fluid">
- 			<div class="nav">
+
+
+ 		<div class="container-fluid" style="border: 1px solid lightgray;">
+ 			<div class="nav-bar">
  				<div class="nav-item">
  					<h1>nav</h1>
  				</div>
  			</div>
  		</div>
 
- 		<div class="container">
+ 		<!-- <div class="reserveren" style="position: sticky; top: 10%;">
+ 			<div class="col-md-2 bg-dark rounded text-white" style="min-height: 80vh;">
+ 				<h1>reserveren</h1>
+ 			</div>
+ 		</div> -->
+
+ 		<div class="reserveren bg-dark" style="position: fixed; top: 10vh; right: 0vw; min-height: 80vh; min-width: 20vw;">test</div>
+
+ 		<div class="container-fluid" >
  			<div class="row">
  				<div class="col-md-12 text-center">
  					<h1>George marina</h1>
  				</div>
- 				<div class="col-md-6 text-center border" style="min-height: 60vh;">
+ 				<div class="col-md-3 offset-md-1 text-center border">
  					<h1>left plane</h1>
  				</div>
- 				<div class="col-md-6 text-center border" style="min-height: 60vh;">
+ 				<div class="col-md-4 text-center border">
+ 					<h1>mid plane</h1>
+ 				</div>
+ 				<div class="col-md-3 text-center border">
  					<h1>right plane</h1>
  				</div>
  			</div>
