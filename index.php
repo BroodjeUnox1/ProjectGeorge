1e20a8860298980b793350e90a7abb2ae5a03015
BroodjeUnox1 <stevenwoody7@gmail.com>
2021-09-21 16:27:30 +0200

    almost done george marina

--- index.php
+++ index.php
@@ -1,100 +1,128 @@
 <?php 
-
-
-
-
-
- ?>
-
-
+    ?>
 <!DOCTYPE html>
-	<html lang="en">
- 	<head>
- 		<!-- Page title -->
-   		<title>George | Homepage</title>
-   		<!-- meta tags -->
-   		<meta charset="utf-8">
-   		<meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+    <head>
+        <!-- Page title -->
+        <title>George | Homepage</title>
+        <!-- meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--  link & cdn's || bootstrap css -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <!-- style.css -->
+        <link rel="stylesheet" type="text/css" href="homepage.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- popper -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <!-- bootstrap js -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- fontawsome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <!-- header -->
+        <div class="container text-center border-black-bottom pt-3 pb-3">
+            <h6>Click here for the latest covid-19 updates.</h6>
+        </div>
+        <!-- brand logo -->
+        <div class="container text-center border-black-bottom pt-3 pb-3">
+            <img src="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/5f3ece93689659584f431736_Marina%202.svg">
+        </div>
+        <!-- mid section  -->
+        <div class="container">
+            <div class="row mt-3">
+                <!-- linker kant eerte gedeelte -->
+                <div class="col-md-3 text-center">
+                    <h6>
+                        OPENING<br>
+                        HOURS<br><br>
+                        OPEN DAILY FROM<br>
+                        11A.M. TILL LATE<br><br>
+                        <small>We'll rock your boat at George Marina</small><br><br>
+                        <small>On sunny days we have valet parking for a fee of <u>€12,50</u> during lunch and <u>€15</u> during dinner</small>
+                        <!-- linker kant 2 de gedeele -->
+                        <hr style="border-bottom: 1px solid black;">
+                        <img src="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg" loading="lazy" sizes="(max-width: 479px) 200px, (max-width: 767px) 39vw, (max-width: 991px) 200px, (max-width: 1919px) 19vw, 200px" height="140" srcset="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-500.jpeg 500w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-800.jpeg 800w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg 938w" alt="" class="image-12">
+                        <b>CONTACT</b><br><br>
+                        MAPS<br>
+                        E-MAIL<br>
+                        PHONE
+                    </h6>
+                </div>
+                <!-- midden stuk -->
+                <div class="col-md-6 text-center border-black-left border-black-right">
+                    <h6>
+                        THIS IS US<br><br><br>
+                        George Marina is set in a spectacular, three storey waterside building in the upcoming Amstelkwartier, right along the banks of the Amstel river that flows into Amsterdam's iconic canals.
+                        <br>
+                        <br>
+                        With large windows overlooking the marina in front, an open kitchen and sushi counter, lush plants, a striking 360 green marble bar and an eye-catching staircase right in the middle of the restaurant, it's also our cuisine that will leave a long lasting impression. Being so close to the water, we're famous for our seafood and sushi platters, but you might as well order steak frites, a juicy bistro burger or a veggie dish. A great all-day hangout for breakfast, lunch, dinner or Friday night cocktails accompanied by a live set from our resident DJ.
+                        <br>
+                        <br>
+                        Despite our restaurants' modern allure, our large, sunny terrace has more of a Côte d'Azur feel – especially in the summer when guests coming from the canals dock their boats in the marina, and enjoy a drink while watching the sunset. No matter what season and what time of the day though, George Marina is a great place to escape hectic, daily life – if only for one day.   
+                        - fin -
+                    </h6>
+                </div>
+                <!-- Rechter kant eerste gedeelte -->
+                <div class="col-md-3 text-center">
+                    <h3>
+                        MENU'S
+                        <br>
+                        <br>
+                        FOOD
+                        <br>
+                        DRINKS
+                        <br>
+                        <br>
+                        <!-- rechter kant tweede gedeelte -->
+                        <hr style="border-bottom: 1px solid black;">
+                        <br>
+                        <b>BOOK</b>
+                        <br>
+                        <b>MY</b>
+                        <br>
+                        <b>TABLE</b>
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <!-- reserveren -->
+        <div class="reserveren bg-dark rounded">
+            <div class="icon">
+                <i class="far fa-window-close fa-2x icon2" style="padding: 7px; color: whitesmoke;" onclick="sidebar()"></i>
+            </div>
+            <div class="container-fluid text-white text-center reserverenElements">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Reserveren</h1>
+                    </div>
+                    <div class="col-md-12 text-left">
+                        <small>Aantal personen:</small>
+                        <select class="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                        </select>
+                    </div>
+                    <div class="col-md-12">
+                        <h1>datum</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-   		<!--  link & cdn's || bootstrap css -->
-   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-   		<!-- style.css -->
-   		<link rel="stylesheet" type="text/css" href="homepage.css">
-   		<!-- jQuery -->
-   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-   		<!-- popper -->
-   		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-   		<!-- bootstrap js -->
-   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-   		<!-- fontawsome -->
-   		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
- 	</head>
- 	<body>
-
- 		<div class="container-fluid" style="border: 1px solid lightgray;">
- 			<div class="nav-bar">
- 				<div class="nav-item">
- 					<h1>nav</h1>
- 				</div>
- 			</div>
- 		</div>
-
-
- 		<div class="reserveren bg-dark rounded">
- 			<div class="icon">
- 				<i class="far fa-window-close fa-2x icon2" style="padding: 7px; color: whitesmoke;" onclick="sidebar()"></i>
- 			</div>
- 			<div class="container-fluid text-white text-center reserverenElements">
- 				<div class="row">
- 					<div class="col-md-12">
- 						<h1>Reserveren</h1>
- 					</div>
- 					<div class="col-md-12 text-left">
- 						<small>Aantal personen:</small>
- 						<select class="form-control">
- 							<option>1</option>
- 							<option>2</option>
- 							<option>3</option>
- 							<option>4</option>
- 							<option>5</option>
- 							<option>6</option>
- 							<option>7</option>
- 							<option>8</option>
- 							<option>9</option>
- 							<option>10</option>
- 						</select>
- 					</div>
- 					<div class="col-md-12">
- 						<h1>datum</h1>
- 					</div>
- 				</div>
- 			</div>
- 		</div>
-
- 		<div class="container-fluid" >
- 			<div class="row">
- 				<div class="col-md-12 text-center">
- 					<h1>George marina</h1>
- 				</div>
- 				<div class="col-md-3 offset-md-1 text-center border">
- 					<h1>left plane</h1>
- 				</div>
- 				<div class="col-md-4 text-center border">
- 					<h1>mid plane</h1>
- 				</div>
- 				<div class="col-md-3 text-center border">
- 					<h1>right plane</h1>
- 				</div>
- 			</div>
- 		</div>
-
- 		
-
- 		<!-- link js scripts -->
- 		<script src="main.js"></script>
- 		<!-- <script type="text/javascript">
- 			
- 		</script> -->
- 	</body>
- </html>
+        <!-- link js scripts -->
+        <script src="main.js"></script>
+        <!-- <script type="text/javascript">
+            </script> -->
+    </body>
+</html>
