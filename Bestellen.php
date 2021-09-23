4865c3a3c9aed56699a72f24604a45d839f5691e
BroodjeUnox1 <stevenwoody7@gmail.com>
2021-09-23 14:06:45 +0200

    added basic shoppingbasket

---
+++ Bestellen.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+include "./class/bestelClass.php";
+$test= new Bestel();
+
+if(isset($_POST["add"])) {
+    // print($_POST['currency']);
+    // print($_POST['name']);
+    $test->add($_POST['name'], $_POST['currency']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Page title -->
+        <title>George | Bestellen</title>
+        <!-- meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--  link & cdn's || bootstrap css -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <!-- style.css -->
+        <link rel="stylesheet" type="text/css" href="homepage.css">
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- popper -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <!-- bootstrap js -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- fontawsome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <?php $test->show(); ?>
+    </body>
+</html>
---
+++ class/bestelClass.php
@@ -0,0 +1,91 @@
+<?php 
+
+/**
+ * 
+ */
+class Bestel
+{	
+	public $data = [
+	[
+		"currency" => "$46.61",
+		"name" => "Burger"
+	],
+	[
+		"currency" => "$4.89",
+		"name" => "Cheesebruger"
+	],
+	[
+		"currency" => "$77.06",
+		"name" => "counter pounter"
+	],
+	[
+		"currency" => "$45.67",
+		"name" => "Big mac"
+	],
+	[
+		"currency" => "$10.95",
+		"name" => "Big tasty"
+	]
+];
+	public $html;
+
+	public $total;
+	
+	function __construct()
+	{	
+		
+		// var_dump($_SESSION["mandje"]);
+		if (isset($_SESSION["mandje"])) {
+			$this->total();
+		}else {
+			$_SESSION['mandje'] = array();
+		}
+	}
+
+	public function show(){
+		$this->html = '<div class="container-fluid">';
+		$this->html .= '<div class="col-md-12 text-center"><h1>Menu</h1></div>';
+		$this->html .= '<div class="col-md-6 offset-md-3">';
+		$this->html .= '<div class="row">';
+
+		foreach ($this->data as $key) {
+			$this->html .= '<div class="col-md-6">';
+			$this->html .= '<form method="POST">';
+			$this->html .= '<div class="card mt-5">';
+			$this->html .= '<div class="card-header">' . $key["name"]. '</div>';
+			$this->html .= '<div class="card-body"><img src="https://some.placeholder/150"></div>';
+			$this->html .= '<div class="card-footer">
+								<div class="row">
+									<div class="col-md-6">' . $key["currency"] . '</div>
+									<div class="col-md-6 text-right">
+										<button class="btn btn-success" type="submit" name="add">add</button>
+									</div>
+								</div>
+							</div>';
+			$this->html .= '<input type="hidden" value="'.$key["name"].'" name="name"></input>';
+			$this->html .= '<input type="hidden" value="'.$key["currency"].' " name="currency"></input>';
+			$this->html .= '</form></div></div>';
+		}
+		$this->html .= '</div>';
+		$this->html .= '</div>';
+		print($this->html);
+	}
+
+	public function add($name, $currency) {
+		$data = ["name" => $name,"currency" => $currency];
+		array_push($_SESSION['mandje'], $data);
+	
+		
+	}
+
+	public function total() {
+		foreach ($_SESSION['mandje'] as $key) {
+			$this->total += str_replace("$", '', $key["currency"]);
+		}
+		print('total: $'.$this->total);
+	}
+
+
+}
+
+ ?>
