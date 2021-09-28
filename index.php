4a07930c1bdd6ec39829403df57b5d41c06b29f1
kapiteinsteef <330176@student.mboutrecht.nl>
2021-09-28 16:47:54 +0200

    Added styling for home page

--- index.php
+++ index.php
@@ -21,6 +21,10 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <!-- fontawsome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Baskervville&display=swap" rel="stylesheet">
     </head>
     <body>
         <!-- header -->
@@ -36,35 +40,48 @@
             <div class="row mt-3">
                 <!-- linker kant eerte gedeelte -->
                 <div class="col-md-3 text-center">
-                    <h6>
+                    <h6 class="heading_2">
                         OPENING<br>
                         HOURS<br><br>
+                    </h6>
+                    <h6 class="about_text">
                         OPEN DAILY FROM<br>
                         11A.M. TILL LATE<br><br>
-                        <small>We'll rock your boat at George Marina</small><br><br>
-                        <small>On sunny days we have valet parking for a fee of <u>€12,50</u> during lunch and <u>€15</u> during dinner</small>
-                        <!-- linker kant 2 de gedeele -->
-                        <hr style="border-bottom: 1px solid black;">
-                        <img src="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg" loading="lazy" sizes="(max-width: 479px) 200px, (max-width: 767px) 39vw, (max-width: 991px) 200px, (max-width: 1919px) 19vw, 200px" height="140" srcset="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-500.jpeg 500w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-800.jpeg 800w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg 938w" alt="" class="image-12">
-                        <b>CONTACT</b><br><br>
+                    </h6>
+                    
+                    <small>We'll rock your boat at George Marina</small><br><br>
+                    <small>On sunny days we have valet parking for a fee of <u>€12,50</u> during lunch and <u>€15</u> during dinner</small>
+                    <!-- linker kant 2 de gedeele -->
+                    <hr style="border-bottom: 1px solid black;">
+                    <img src="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg" loading="lazy" sizes="(max-width: 479px) 200px, (max-width: 767px) 39vw, (max-width: 991px) 200px, (max-width: 1919px) 19vw, 200px" height="140" srcset="https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-500.jpeg 500w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina-p-800.jpeg 800w, https://uploads-ssl.webflow.com/5f3ece93689659d6e7431728/60d1aff9974cae25741692a3_marina.jpg 938w" alt="" class="image-12">
+                    <b>CONTACT</b><br><br>
                         MAPS<br>
                         E-MAIL<br>
                         PHONE
-                    </h6>
+                    
                 </div>
                 <!-- midden stuk -->
                 <div class="col-md-6 text-center border-black-left border-black-right">
-                    <h6>
-                        THIS IS US<br><br><br>
-                        George Marina is set in a spectacular, three storey waterside building in the upcoming Amstelkwartier, right along the banks of the Amstel river that flows into Amsterdam's iconic canals.
+                    
+                        <h6 class="Heading_about">
+                                THIS IS US
+                        </h6>
+                        <br><br>
+                        <h6 class="about_text">
+                                George Marina is set in a spectacular, three storey waterside building in the upcoming Amstelkwartier, right along the banks of the Amstel river that flows into Amsterdam's iconic canals.
+                        </h6>
                         <br>
+                        <h6 class="about_text">
+                            With large windows overlooking the marina in front, an open kitchen and sushi counter, lush plants, a striking 360 green marble bar and an eye-catching staircase right in the middle of the restaurant, it's also our cuisine that will leave a long lasting impression. Being so close to the water, we're famous for our seafood and sushi platters, but you might as well order steak frites, a juicy bistro burger or a veggie dish. A great all-day hangout for breakfast, lunch, dinner or Friday night cocktails accompanied by a live set from our resident <b>DJ</b>.
+                        </h6>
                         <br>
-                        With large windows overlooking the marina in front, an open kitchen and sushi counter, lush plants, a striking 360 green marble bar and an eye-catching staircase right in the middle of the restaurant, it's also our cuisine that will leave a long lasting impression. Being so close to the water, we're famous for our seafood and sushi platters, but you might as well order steak frites, a juicy bistro burger or a veggie dish. A great all-day hangout for breakfast, lunch, dinner or Friday night cocktails accompanied by a live set from our resident DJ.
-                        <br>
-                        <br>
-                        Despite our restaurants' modern allure, our large, sunny terrace has more of a Côte d'Azur feel – especially in the summer when guests coming from the canals dock their boats in the marina, and enjoy a drink while watching the sunset. No matter what season and what time of the day though, George Marina is a great place to escape hectic, daily life – if only for one day.   
-                        - fin -
-                    </h6>
+                        <h6 class="about_text">
+                            Despite our restaurants' modern allure, our large, sunny terrace has more of a Côte d'Azur feel – especially in the summer when guests coming from the canals dock their boats in the marina, and enjoy a drink while watching the sunset. No matter what season and what time of the day though, George Marina is a great place to escape hectic, daily life – if only for one day.   
+                        </h6>
+                        <em>
+                            <b>- fin -</b>
+                        </em>
+                            
                 </div>
                 <!-- Rechter kant eerste gedeelte -->
                 <div class="col-md-3 text-center">
@@ -77,15 +94,17 @@
                         <a id="drinks" href="./documents/610a902eb3d85f07df110d35_Marina drinks.pdf" download>DRINKS</a>
                         <br>
                         <br>
-                        <!-- rechter kant tweede gedeelte -->
-                        <hr style="border-bottom: 1px solid black;">
-                        <br>
-                        <b>BOOK</b>
-                        <br>
-                        <b>MY</b>
-                        <br>
-                        <b>TABLE</b>
                     </h3>
+                    <!-- rechter kant tweede gedeelte -->
+                    <hr style="border-bottom: 1px solid black;">
+                    <br>
+                    <div class="blackToWhite">
+                        <h3><b>BOOK</b></h3>   
+                        <h3><b>MY</b></h3>
+                        <h3><b>TABLE</b></h3>
+                    </div>
+                    
+                    
                 </div>
             </div>
         </div>
