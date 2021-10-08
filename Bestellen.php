71208c216d22118d411d68426211391735132a9a
tyra cudjoe <tyra20015@gmail.com>
2021-10-08 10:40:33 +0200

    added styling, navbar, fonts to bestellen

--- Bestellen.php
+++ Bestellen.php
@@ -11,35 +11,100 @@ if(isset($_POST["add"])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- Page title -->
-        <title>George | Bestellen</title>
-        <!-- meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!--  link & cdn's || bootstrap css -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-        <!-- style.css -->
-        <link rel="stylesheet" type="text/css" href="homepage.css">
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <!-- popper -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-        <!-- bootstrap js -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-        <!-- fontawsome -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    </head>
-    <body>
-        <?php $test->show(); ?>
-    </body>
-    <script type="text/javascript">
-        function add(val1, val2){
-            $.post('./api/bestel.php', {name: val1, currency: val2}, function(response){
-                console.log(response)
-                location.reload();
-            })
-        }
-    </script>
+
+<head>
+    <!-- Page title -->
+    <title>George | Bestellen</title>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--  link & cdn's || bootstrap css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- style.css -->
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- popper -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yaldevi&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container" id="menu">
+        <h1><strong>Menu</strong></h1>
+    </div>
+
+    <div class="container mt-2">
+        <nav class="navbar navbar-expand-lg navbar-light" id="menuNavbar">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container mt-2 category">
+        <h3><strong>Category A</strong></h3>
+    </div>
+
+    <?php $test->show(); ?>
+</body>
+
+<script type="text/javascript">
+    function add(val1, val2) {
+        $.post('./api/bestel.php', {
+            name: val1,
+            currency: val2
+        }, function (response) {
+            console.log(response)
+            location.reload();
+        })
+    }
+</script>
+
+<script type="text/javascript">
+    let header = document.getElementById('menuNavbar');
+    let buttons = header.getElementsByClassName('nav-link')
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function () {
+            let current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        })
+    }
+</script>
 
 </html>
--- class/bestelClass.php
+++ class/bestelClass.php
@@ -1,85 +1,91 @@
 <?php 
 
-class Bestel
-{	
-	public $data = [
-	[
-		"currency" => "$46.61",
-		"name" => "Burger"
-	],
-	[
-		"currency" => "$4.89",
-		"name" => "Cheesebruger"
-	],
-	[
-		"currency" => "$77.06",
-		"name" => "counter pounter"
-	],
-	[
-		"currency" => "$45.67",
-		"name" => "Big mac"
-	],
-	[
-		"currency" => "$10.95",
-		"name" => "Big tasty"
-	]
-];
-	public $html;
-
-	public $total;
-	
-	function __construct()
+	class Bestel
 	{	
+		public $data = [
+		[
+			"currency" => "$46.61",
+			"name" => "Burger",
+			"omschrijving" => "allerlekkerste burger"
+		],
+		[
+			"currency" => "$4.89",
+			"name" => "Cheesebruger",
+			"omschrijving" => "allerlekkerste cheeseburger"
+
+		],
+		[
+			"currency" => "$77.06",
+			"name" => "counter pounter",
+			"omschrijving" => "allerlekkerste counter pounter"
+		],
+		[
+			"currency" => "$45.67",
+			"name" => "Big mac",
+			"omschrijving" => "allerlekkerste big mac"
+		],
+		[
+			"currency" => "$10.95",
+			"name" => "Big tasty",
+			"omschrijving" => "allerlekkerste big tasty"
+		]
+	];
+		public $html;
+
+		public $total;
 		
-		// var_dump($_SESSION["mandje"]);
+		function __construct()
+		{	
+			
+			// var_dump($_SESSION["mandje"]);
 
-		if (isset($_SESSION["mandje"])) {
-			$this->total();
-		}else {
-			$_SESSION['mandje'] = array();
+			if (isset($_SESSION["mandje"])) {
+				$this->total();
+			}else {
+				$_SESSION['mandje'] = array();
+			}
 		}
-	}
 
-	public function show(){
-		$this->html = '<div class="container-fluid">';
-		$this->html .= '<div class="col-md-12 text-center"><h1>Menu</h1></div>';
-		$this->html .= '<div class="col-md-6 offset-md-3">';
-		$this->html .= '<div class="row">';
-
-		foreach ($this->data as $key) {
-			$this->html .= '<div class="col-md-6">';
-			// $this->html .= '<form method="POST">';
-			$this->html .= '<div class="card mt-5">';
-			$this->html .= '<div class="card-header">' . $key["name"]. '</div>';
-			$this->html .= '<div class="card-body text-center"><img src="https://www.karlijnskitchen.com/wp-content/uploads/2015/09/cheese-bacon-burger-1-e1473968135128-150x150.jpg"></div>';
-			$this->html .= '<div class="card-footer">
-								<div class="row">
-									<div class="col-md-6">' . $key["currency"] . '</div>
-									<div class="col-md-6 text-right">
-										<button class="btn btn-success" name="add" onclick="add(`'.$key["name"]. '`, `'.$key["currency"].'`)">add</button>
-									</div>
-								</div>
-							</div>';
-			$this->html .= '</div></div>'; 
-			// </form>
+		public function show(){
+				$this->html = '<div class="container mt-5">';
+			
+				foreach ($this->data as $key) {
+					$this->html .= '<div class="menuCard row">';
+					$this->html .= '<div class="col-5 col-xs-7 col-sm-4 col-md-3 col-lg-3 col-xl-2 col-xxl-2">';
+					// $this->html .= '<form method="POST">';
+					$this->html .= '<img class="" id="testImage" src="https://www.karlijnskitchen.com/wp-content/uploads/2015/09/cheese-bacon-burger-1-e1473968135128-150x150.jpg" alt="...">';
+					$this->html .= '</div>';
+					$this->html .= '<div class="col-7 col-xs-5 col-sm-8 col-md-9 col-lg-9 col-xl-10 col-xxl-10 nameOmschrijving">
+										<h3>'. $key["name"] .'</h3>
+										<p>'. $key["omschrijving"] .'</p>
+									</div>';
+					$this->html .= '<div class="col-4 col-xs-3 col-sm-3 col-md-2 col-lg-2 col-xl-2 col-xxl-1 button">
+										<p><strong>'. $key["currency"] .'</strong></p>
+										<button type="button" class="btn btn-secondary btn-block">+</button>
+									</div>';
+									// $this->html .= '<div class="" style="float: right;">' . $key["name"]. '</div>';
+									// </form>
+					$this->html .= '</div>';
+				}
+				
+				$this->html .= '</div> <br>'; 
+				print($this->html);
 		}
-		$this->html .= '</div>';
-		$this->html .= '</div>';
-		print($this->html);
-	}
+		
 
-	public function add($name, $currency) {
-		$data = ["name" => $name,"currency" => $currency];
-		array_push($_SESSION['mandje'], $data);	
+		public function add($name, $currency) {
+			$data = ["name" => $name,"currency" => $currency];
+			array_push($_SESSION['mandje'], $data);	
+		}
+		
+
+		public function total() {
+			foreach ($_SESSION['mandje'] as $key) {
+				$this->total += (float)str_replace("$", '', $key["currency"]);
+			}
+			print('total: $' .$this->total);
+		}
 	}
 	
 
-	public function total() {
-		foreach ($_SESSION['mandje'] as $key) {
-			$this->total += (float)str_replace("$", '', $key["currency"]);
-		}
-		print('total: $' .$this->total);
-	}
-}
-
- ?>
+?>
